de857106bab6c284bea48ffefb5520fafb4804c7
Toshiba <Toshiba@DESKTOP-9D2RVK6>
2024-08-12 12:03:57 +0800

    Add project files.

---
+++ CMakeProject01/CMakeProject01.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject01.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject01.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
